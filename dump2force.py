651bf8ae28d09ff89a61fc2b963c96b886e7bea3
msbentley@gmail.com <msbentley@gmail.com@96a47f26-baa7-e970-4d3b-8e50843cf4f4>
2011-10-07 11:39:25 +0000

    Added support for periodic boundaries, contact area and heat transfer

--- dump2force.py
+++ dump2force.py
@@ -12,6 +12,11 @@ therefore PYTHONPATH must include the pizza/src location.
 
 NOTE: bdump is NOT included in granular pizza, and should be taken
 from the standard LAMMPS pizza package!
+
+NOTE: it is impossible to tell from the bdump header which values
+have been requested in the compute, so check that your compute
+and dump match the format here - this will be checked in future!
+
 """
 
 from evtk.vtk import VtkFile, VtkGroup, VtkUnstructuredGrid
@@ -19,11 +24,9 @@ from bdump import bdump
 import numpy as np
 import sys, os
 
-
-# TODO: use a try/except here to check for missing modules, and report
-# TODO: ask for timestep or timestep range as input
-# TODO: check for periodic boundaries and avoid connecting points that map across them
-
+# TODO: use a try/except here to check for missing modules, and fallback to ASCII VTK if evtk not found
+# TODO: ask for timestep or timestep range as input (code is NOT efficient and large files = long runtimes!)
+# TODO: write celldata for contact area and heat flux (if present)
 
 # Check for command line arguments
 if len(sys.argv) != 2:
@@ -64,18 +67,25 @@ groupfile = VtkGroup(groupfile)
 fileindex = 0
 timestep = forcedata.next()
 
-# check that we have the right number of colums (11)
-if forcedata.snaps[fileindex].natoms !=0 and len(forcedata.snaps[0].atoms[0]) != 11:
-    print "Error - dump file requires all parameters from a compute pair/gran/local id pos force (11 in total)"
-    sys.exit
+# check that we have the right number of colums (>11)
+#
+# NOTE: the first timesteps are often blank, and then natoms returns 0, so this doesn't really work...
+#
+if forcedata.snaps[fileindex].natoms !=0 and len(forcedata.snaps[0].atoms[0]) < 11:
+    print "Error - dump file requires at least all parameters from a compute pair/gran/local id pos force (12 in total)"
+    sys.exit()
 
 # loop through available timesteps
 # 
 while timestep >= 0:
 
-    # data are stored as pos1 (3) pos2 (3) id1 id2 force (3) for 11 columns
+    # default data are stored as pos1 (3) pos2 (3) id1 id2 periodic_flag force (3) -> 12 columns
+    #
+    # if contactArea is enabled, that's one more (13) and heatflux (14)
+    #
     # assign names to atom columns (1-N)
-    forcedata.map(1,"x1",2,"y1",3,"z1",4,"x2",5,"y2",6,"z2",7,"id1",8,"id2",9,"fx",10,"fy",11,"fz")
+    # forcedata.map(1,"x1",2,"y1",3,"z1",4,"x2",5,"y2",6,"z2",7,"id1",8,"id2",9,"periodic",10,"fx",11,"fy",12,"fz")
+    forcedata.map(1,"x1",2,"y1",3,"z1",4,"x2",5,"y2",6,"z2",7,"id1",8,"id2",9,"periodic",10,"fx",11,"fy",12,"fz",13,"area",14,"heatflux")
 
     # check for contact data (some timesteps may have no particles in contact)
     #
@@ -106,6 +116,13 @@ while timestep >= 0:
         # number of cells = number of interactions (i.e. entries in the dump file)
         ncells = len(forcedata.snaps[fileindex].atoms)
 
+        # number of periodic interactions
+        periodic = np.array(forcedata.snaps[fileindex].atoms[:,forcedata.names["periodic"]],dtype=bool)
+        nperiodic = sum(periodic)
+
+        # number of non-periodic interactions (which will be written out)
+        nconnex = ncells - nperiodic
+
         # extract the IDs as an array of integers
         id1 = np.array(forcedata.snaps[fileindex].atoms[:,forcedata.names["id1"]],dtype=long)
         id2 = np.array(forcedata.snaps[fileindex].atoms[:,forcedata.names["id2"]],dtype=long)
@@ -131,7 +148,7 @@ while timestep >= 0:
         y = np.zeros( npoints, dtype=np.float64)
         z = np.zeros( npoints, dtype=np.float64)
 
-        print 'Timestep:',str(timestep),'npoints=',str(npoints),'ncells=',str(ncells)
+        print 'Timestep:',str(timestep),'npoints=',str(npoints),'ncells=',str(ncells),'nperiodic=',nperiodic
 
         # Point data = location of each unique particle
         #
@@ -161,30 +178,45 @@ while timestep >= 0:
         # the order of the ids array, so now we loop through 0..ncells and have to connect 
         # id1 and id2, so I need to see where in ids these correspond to
 
-        # create an empty array to hold particle pairs
-        connections = np.zeros( 2*ncells, dtype=int )
+        # If the periodic flag is set for a given interactions, DO NOT connect the points
+        # (to avoid lines that cross the simulation domain)
+            
+        # Mask out periodic interactions from the cell (connectivity) array
+        # newList = [word for (word, mask) in zip(s,b) if mask]
+        id1_masked = [ident for (ident,mask) in zip(id1,np.invert(periodic)) if mask]
+        id2_masked = [ident for (ident,mask) in zip(id2,np.invert(periodic)) if mask]
 
-        for pair in range(ncells):
-            connections[2*pair],connections[2*pair+1] = ids.index(id1[pair]),ids.index(id2[pair])
+        # create an empty array to hold particle pairs
+        connections = np.zeros( 2*nconnex, dtype=int )
+
+        for pair in range(nconnex):
+            connections[2*pair],connections[2*pair+1] = ids.index(id1_masked[pair]),ids.index(id2_masked[pair])
             
         # The offset array is simply generated from 2*(1..ncells)
-        offset=(np.arange(ncells,dtype=int)+1)*2
+        offset=(np.arange(nconnex,dtype=int)+1)*2
 
         # The type array is simply ncells x 3 (i.e. a VTKLine type)
-        celltype = np.ones(ncells,dtype=int)*3
+        celltype = np.ones(nconnex,dtype=int)*3
 
         # Finally we need force data for each cell
         force = np.sqrt( np.array(forcedata.snaps[fileindex].atoms[:,forcedata.names["fx"]],dtype=np.float64)**2 + \
                          np.array(forcedata.snaps[fileindex].atoms[:,forcedata.names["fy"]],dtype=np.float64)**2 + \
                          np.array(forcedata.snaps[fileindex].atoms[:,forcedata.names["fz"]],dtype=np.float64)**2 )
 
+        # And, optionally, contact area and heat flux (using the same connectivity)
+        area = np.array(forcedata.snaps[fileindex].atoms[:,forcedata.names["area"]],dtype=np.float64)
+        heatflux = np.array(forcedata.snaps[fileindex].atoms[:,forcedata.names["heatflux"]],dtype=np.float64)
+
         # Now we have enough data to create the file:
         # Points - (x,y,z) (npoints)
         # Cells
-        #   Connectivity - connections (ncells,2)
-        #   Offset - offset (ncells)
-        #   type - celltype (ncells)
-        # Celldata - force (ncells)
+        #   Connectivity - connections (nconnex,2)
+        #   Offset - offset (nconnex)
+        #   type - celltype (nconnex)
+        # Celldata
+        #   force    (nconnex)
+        #   area     (nconnex)
+        #   heatflux (nconnex)
 
         # create a VTK unstructured grid (.vtu) file
         vtufile = fileprefix+'_'+str(timestep)
@@ -193,9 +225,9 @@ while timestep >= 0:
         vtufile += '.vtu'
 
         w.openGrid()
-        w.openPiece(npoints=npoints, ncells=ncells)    
+        w.openPiece(npoints=npoints, ncells=nconnex)
 
-        # Set up Points data
+        # Set up Points (x,y,z) data XML
         w.openElement("Points")
         w.addData("points", (x,y,z) )
         w.closeElement("Points")
@@ -203,15 +235,26 @@ while timestep >= 0:
         # Set up Cell data
         w.openElement("Cells")
         w.addData("connectivity", connections )
-
         w.addData("offsets", offset)
         w.addData("types", celltype)
         w.closeElement("Cells")
 
         # Set up force data
-        w.openData("Cell", scalars = "force")
+        w.openData("Cell")
         w.addData("force", force)
+        w.addData("area", area)
+        w.addData("heatflux", heatflux)
         w.closeData("Cell")
+
+        # and contact area
+        # w.openData("Cell", scalars = "area")
+        # w.addData("area", area)
+        # w.closeData("Cell")
+
+        # and heat flux
+        # w.openData("Cell", scalars = "heatflux")
+        # w.addData("heatflux", heatflux)
+        # w.closeData("Cell")
 
         # Wrap up
         w.closePiece()
@@ -220,7 +263,7 @@ while timestep >= 0:
         # Append binary data
         w.appendData( (x,y,z) )
         w.appendData(connections).appendData(offset).appendData(celltype)
-        w.appendData(data = force)
+        w.appendData(force).appendData(area).appendData(heatflux)
         w.save()
 
     # Add this file to the group of all timesteps
